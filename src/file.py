e328acfa8f6d27920a4de3abb8682e0fc441c219
lipebol <lipe_bol5@hotmail.com>
2024-10-28 18:48:27 -0300

    rm arg 'typefile' and some 'returns', changed detect 'json' and 'jpeg'

--- src/file.py
+++ src/file.py
@@ -1,19 +1,18 @@
-
 from inspect import currentframe
 from json import dump, load
-from os import getenv, path
+from os import getenv, path, remove
 from PIL import Image
 from shutil import copyfile
 
 
 class File:
 
-    def __init__(self, dir: str) -> None:
-        self._dir = dir
+    def __init__(self) -> None:
+        self._dir = path.dirname(__file__)
 
-    def read(self, file: str, typefile=None) -> object:
+    def read(self, file: str) -> object:
         try:
-            if typefile == "json":
+            if ".json" in file:
                 return load(open(self._dir + file))
             return open(self._dir + file).read().strip()
         except Exception as error:
@@ -21,12 +20,14 @@ class File:
             return False
     
 
-    def write(self, file: str, content: str or dict or bytes, typefile=None) -> int:
+    def write(self, file: str, content: str or dict or bytes) -> str:
         try:
             self.type_write = 'wb' if type(content) == bytes else 'w'
-            if typefile == "json":
-                return dump(content, open(self._dir + file, 'r+'), indent=5)
-            return open(self._dir + file, self.type_write).write(content)
+            if ".json" in file:
+                dump(content, open(self._dir + file, 'r+'), indent=5)
+            else:
+                open(self._dir + file, self.type_write).write(content)
+            return file
         except Exception as error:
             print(f"{error} in: @{currentframe().f_code.co_name}")
             return False
@@ -34,7 +35,7 @@ class File:
     def exists(self, file: str) -> bool:
         try:
             if not path.isfile(self._dir + file):
-                return self.write(file, getenv('VOID'))
+                self.write(file, getenv('VOID'))
             return True
         except Exception as error:
             print(f"{error} in: @{currentframe().f_code.co_name}")
@@ -43,10 +44,11 @@ class File:
     
     def new(self, source: str, destination: str) -> str:
         try:
-            if "jpeg" in source:
-                Image.open(source).save(destination)
-                return remove(getenv('ORIGINAL_IMAGE'))
-            return copyfile(source, destination)
+            if ".jpeg" in source:
+                Image.open(self._dir + source).save(self._dir + destination)
+                remove(self._dir + getenv('ORIGINAL_IMAGE'))
+                return destination
+            return copyfile(self._dir + source, self._dir + destination)
         except Exception as error:
             print(f"{error} in: @{currentframe().f_code.co_name}")
             return False
