86158f6befae7206dde37709c1b9524856579c50
lipebol <lipe_bol5@hotmail.com>
2025-07-21 00:33:13 -0300

    add lines

--- spark/dataframes.py
+++ spark/dataframes.py
@@ -1,6 +1,7 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import col
 
-spark = SparkSession.builder.master("local[*]").appName("RDDs").getOrCreate()
+spark = SparkSession.builder.master("local[*]").appName("DataFrames").getOrCreate()
 
 file = spark.sparkContext.textFile('./files/sales.txt')
 
@@ -21,5 +22,34 @@ df_from_rdd = sparkDataFrame(
     ), ['name', 'price']
 )
 
-df_from_list_dict.show()
-df_from_rdd.show()
+df_from_list_dict.show(5)
+print(df_from_list_dict.count())
+df_from_rdd.printSchema()
+print(df_from_rdd.count())
+
+df_from_csv = spark.read.csv(
+    './files/sales.csv', header=True, inferSchema=True, sep=';'
+).fillna({"price": 0}).dropna(subset=['name'])
+
+df_from_csv.show(60)
+
+df_from_json = spark.read.json('./files/sales.json', multiLine=True)
+
+df_from_csv.select('name').join(
+    df_from_json, 'name', 'right'
+).write.parquet('./files/sales')
+
+df_from_parquet = spark.read.parquet('./files/sales')
+
+def filter(data):
+    return data.filter(
+        df_from_parquet.price > 900
+    )
+
+filter(df_from_parquet).select('name').show()
+
+df_from_parquet = df_from_parquet.withColumn('cost_value', col('price') - 200)
+
+filter(df_from_parquet).withColumn(
+    'sale_value', col('price') + 200
+).select('name', 'cost_value', 'sale_value').show()
